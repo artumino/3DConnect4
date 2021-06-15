dda3e68c1c6887b166ef8c4c75d09a69b3dcffb5
Jacopo Libè <jacopo.libe@gmail.com>
2021-06-15 19:09:50 +0200

    Start creating game logic

---
+++ scripts/game/connect4.js
@@ -0,0 +1,12 @@
+var Connect4Manager = function()
+{
+    gameScene = undefined;
+}
+
+Connect4Manager.prototype.dropPiece = function(player, row, column)
+{
+    if(gameScene)
+    {
+        
+    }
+}
