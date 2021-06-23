55adff90432cb40bb61bd8c5fbf19a07a8649254
Jacopo Libè <jacopo.libe@gmail.com>
2021-06-23 09:32:59 +0200

    Calculate fragment world position

--- shaders/ubershader/ubershader_vs.glsl
+++ shaders/ubershader/ubershader_vs.glsl
@@ -4,13 +4,16 @@ in vec3 inPosition0;
 in vec3 inNormal0;
 in vec2 inUV0;
 out vec2 uvFS;
+out vec3 fs_pos;
 out vec3 normalVector;
 
 uniform mat4 matrix_MVP;
 uniform mat4 matrix_N;
+uniform mat4 matrix_W;
 
 void main() {
   uvFS = inUV0;
   normalVector = mat3(matrix_N) * inNormal0;
+  fs_pos = matrix_W * inPosition0;
   gl_Position = matrix_MVP * vec4(inPosition0, 1.0);
 }
